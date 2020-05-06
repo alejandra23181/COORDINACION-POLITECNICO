70f69736b48ff46bdf923864bada0da74fe2a1d0
maria <maria_montoya23181@elpoli.edu.co>
2020-05-06 18:03:32 -0500

    Commit rutas y navegacion - MA

--- helpdesk/src/App.js
+++ helpdesk/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Tareas from './containers/Solicitudes'
 import Home from './pages/Home'
 
 function App() {
