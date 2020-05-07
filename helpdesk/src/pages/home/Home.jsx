eedf50f528d7d0de832c85a193cf7a46252e5009
maria <maria_montoya23181@elpoli.edu.co>
2020-05-07 16:15:56 -0500

    Cambios en estilos, container y estructura - MA

--- helpdesk/src/pages/home/Home.jsx
+++ helpdesk/src/pages/home/Home.jsx
@@ -10,16 +10,40 @@ export default function Home() {
             <Carrusel />
             <section className='cp-home-container'>
                 <article className='cp-article' >
-                    <h3>ACERCA DE NOSOTROS</h3>
-                    <p>
-                    Somos un sistema de información que te puede ayudar con estos procesos:
-                    Creación de solicitudes, tareas, usuarios, manipulación de disponibilidad y reportes de
-                    incidentes que se presentan en las aulas de informática,esto con el fin de poder dar solución a
-                    los problemas evidenciados por los usuarios que hacen parte de la comunidad politécnica,
-                    y de esta manera poder disminuir la molestia que se presenta al no dar solución a estos.
-                    </p>
+                    <h3>SERVICIOS Y SOLUCIONES PARA TODA LA COMUNIDAD EDUCATIVA</h3>
                 </article>
-                    
+<div className="cp-contenedor">
+                <div class="row mb-2">
+                    <div class="col-md-6">
+                        <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                            <div class="col p-4 d-flex flex-column position-static">
+                                <strong class="d-inline-block mb-2 text-primary">World</strong>
+                                <h3 class="mb-0">Featured post</h3>
+                                <div class="mb-1 text-muted">Nov 12</div>
+                                <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="stretched-link">Continue reading</a>
+                            </div>
+                            <div class="col-auto d-none d-lg-block">
+                                <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                            <div class="col p-4 d-flex flex-column position-static">
+                                <strong class="d-inline-block mb-2 text-success">Design</strong>
+                                <h3 class="mb-0">Post title</h3>
+                                <div class="mb-1 text-muted">Nov 11</div>
+                                <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="stretched-link">Continue reading</a>
+                            </div>
+                            <div class="col-auto d-none d-lg-block">
+                                <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
             </section>
         </main>
     );
