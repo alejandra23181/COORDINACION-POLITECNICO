eec78a7a8db3c4be022d39c86f340654499933bc
maria <maria_montoya23181@elpoli.edu.co>
2020-05-06 19:15:41 -0500

    Principal - MA

--- helpdesk/src/components/Menu.jsx
+++ helpdesk/src/components/Menu.jsx
@@ -11,11 +11,11 @@ function Menu() {
                     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
                         <h5 className="my-0 mr-md-auto font-weight-normal">Poli Dynamics</h5>
                         <nav className="my-2 my-md-0 mr-md-3">
-                            <Link to={'/'} className="p-2 text-dark">Home</Link>
-                            <Link to={'/'} className="p-2 text-dark">Availability</Link>
-                            <Link to={'/'} className="p-2 text-dark">About us</Link>
-                            <Link to={'/'} className="p-2 text-dark">Who we serve </Link>
-                            <Link to={'/'} className="p-2 text-dark">Our services </Link>
+                            <Link to={'/'} className="p-2 text-dark">Inicio</Link>
+                            <Link to={'/'} className="p-2 text-dark">Disponibilidad</Link>
+                            <Link to={'/'} className="p-2 text-dark">Acerca de</Link>
+                            <Link to={'/'} className="p-2 text-dark">Nuestros servicios</Link>
+                            <Link to={'/'} className="p-2 text-dark">¿A quién servimos?</Link>
                         </nav>
                         <Link to={'/'} className="btn btn-outline-primary" href="#">Sign up</Link>
                     </div>
--- helpdesk/src/containers/Solicitudes.js
+++ helpdesk/src/containers/Solicitudes.js
@@ -13,10 +13,10 @@ function Solicitudes() {
                     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
                     <h5 className="my-0 mr-md-auto font-weight-normal">Poli Dynamics</h5>
                         <nav className="my-2 my-md-0 mr-md-3">
-                            <Link to={'/'} className="p-2 text-dark">Home</Link>
-                            <Link to={'/'} className="p-2 text-dark">Availability</Link>
-                            <Link to={'/Insert'} className="p-2 text-dark">Create requests</Link>
-                            <Link to={'/View'}  className="p-2 text-dark">View requests</Link>
+                            <Link to={'/'} className="p-2 text-dark">Inicio</Link>
+                            <Link to={'/'} className="p-2 text-dark">Disponibilidad</Link>
+                            <Link to={'/Insert'} className="p-2 text-dark">Nueva solicitud</Link>
+                            <Link to={'/View'}  className="p-2 text-dark">Listado de solicitudes</Link>
                         </nav>
                         <Link to={'/'} className="btn btn-outline-primary" href="#">My profile</Link>
                     </div>
