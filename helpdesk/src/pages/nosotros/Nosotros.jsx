8d3ee565b62c8661f7326f29bf03b5a2dd5180e5
maria <maria_montoya23181@elpoli.edu.co>
2020-05-07 17:22:24 -0500

    Cambios en estilos, container y estructura - MA

--- helpdesk/src/pages/nosotros/Nosotros.jsx
+++ helpdesk/src/pages/nosotros/Nosotros.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import '../../components/styles/Nosotros.scss';
+import Presentacion from "../../components/Presentacion/Presentacion";
 import C1 from '../../components/images/nosotros/mision.PNG';
-import C2 from '../../components/images/nosotros/log.jpg';
-import C3 from '../../components/images/nosotros/vision.PNG';
+import C2 from '../../components/images/nosotros/vision.PNG';
 
 export default function Nosotros() {
-    const array = [C1, C2, C3];
+    const data = [
+        { image: C1, title: 'MISIÓN', content: 'Somos una Institución de educación superior estatal de vocación tecnológica, que con su talento humano ofrece una formación integral con programas de calidad en pregrado y posgrado, apoyados en la gestión del conocimiento de base científica; promovemos acciones innovadoras desde la investigación y la proyección social, para contribuir al desarrollo económico, social y ambiental de Antioquia y Colombia.' }
+        , { image: C2, title: 'VISIÓN', content: 'El Politécnico Colombiano Jaime Isaza Cadavid, siempre será reconocido como una institución de alta calidad académica con énfasis en la formación y gestión tecnológica, la investigación aplicada y la proyección social, en beneficio del desarrollo económico, social y ambiental, con presencia en las regiones de Antioquia y el País; articulado a las dinámicas del sector productivo, a la política pública y al crecimiento de la cobertura en educación.' }
+    ]
+
     return (
+
         <main className="cp-nosotros" >
             <div className="cp-container" >
                 <article className='cp-article' >
-                    <h2>ACERCA DE NOSOTROS</h2>
+                    <h4>ACERCA DE NOSOTROS</h4>
                 </article>
-                {array.map((item, key) => <img src={item} key={key} />)}
+                <div className="cp-nuevos">
+                    <Presentacion data={data} />
+                </div>
             </div>
         </main>
+
     )
 }
